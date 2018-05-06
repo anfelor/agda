32dc9e8d70c4555ac799dac8fc4af43af3c00be7
Anton Felix Lorenzen <anfelor@posteo.de>
2018-05-06 19:33:05 +0200

    Make verify.sh return the right exit code

--- verify.sh
+++ verify.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
 rm -rf index.agda;
 echo "module index where" >> index.agda;
 for i in $( find src -name "*.agda" | sed 's/src\/\(.*\)\.agda/\1/' | sed 's/\//\./g' | sort ); do
     echo "import $i" >> index.agda;
 done;
-agda -i . -i src/ index.agda
+agda -i . -i src/ index.agda;
+RET=$?;
 rm -rf index.agda;
+exit $RET;
